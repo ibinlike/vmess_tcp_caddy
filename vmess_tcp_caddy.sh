44c1ae46c9be5f58025c30c831e747e2cf86c9e0
Kevin Li <ibinlike@gmail.com>
2020-04-03 11:18:55 +0800

    'init commit'

---
+++ vmess_tcp_caddy.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+echo "Step 1: Install Haproxy 2.1"
+apt-get install software-properties-common
+add-apt-repository ppa:vbernat/haproxy-2.1
+apt update
+apt-get install haproxy=2.1.\*
+echo "Pull haproxy configuration file and save it to /etc/haproxy/haproxy.cfg"
+wget 'haproxy.cfg' /etc/haproxy/haproxy.cfg
+
+echo 'Step 2: Install v2ray'
+bash <(curl -L -s https://install.direct/go.sh)
+wget 'config.json' /etc/v2ray/config.json
+
+echo 'Step 3: Install Caddy'
+curl https://getcaddy.com | bash -s personal
+
+echo 'Configure Caddy'
+sudo chown root:root /usr/local/bin/caddy
+sudo chmod 755 /usr/local/bin/caddy
+sudo setcap 'cap_net_bind_service=+ep' /usr/local/bin/caddy
+sudo mkdir /etc/caddy
+sudo chown -R root:root /etc/caddy
+sudo mkdir /etc/ssl/caddy
+sudo chown -R root:www-data /etc/ssl/caddy
+sudo chmod 0770 /etc/ssl/caddy
+wget 'caddyfile' 
+sudo cp caddyfile /etc/caddy/
+sudo chown root:root /etc/caddy/Caddyfile
+sudo chmod 644 /etc/caddy/Caddyfile
+wget 'exmple.com' 
+sudo cp example.com /var/www/
+sudo chown -R www-data:www-data /var/www/example.com
+sudo chmod -R 555 /var/www/example.com
+
+wget 'caddy.service'
+sudo cp caddy.service /etc/systemd/system/
+sudo chown root:root /etc/systemd/system/caddy.service
+sudo chmod 644 /etc/systemd/system/caddy.service
+sudo systemctl daemon-reload
+sudo systemctl start caddy.service
+
+sudo systemctl enable caddy.service
+journalctl --boot -u caddy.service
+
+echo 'Step 4: Install Caddy'
+cd /etc/ssl/privates
+apt-get install socat -y
+curl  https://get.acme.sh | sh
+cd /root/.amce.sh
+
+export CF_Key="刚刚保存下来的KEY"
+export CF_Email="cloudflare的注册邮箱"
+acme.sh --issue --dns dns_cf -d *.你的域名.com -d 你的域名.com --keylength ec-256
+
+cat ca.crt ca.key > example.com.pem
