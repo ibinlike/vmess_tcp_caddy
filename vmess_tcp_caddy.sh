4c8adf08b38d50e69392dd72e7226fbfc7142f24
Kevin Li <ibinlike@gmail.com>
2020-04-05 13:32:20 +0800

    'update bugs

--- vmess_tcp_caddy.sh
+++ vmess_tcp_caddy.sh
@@ -35,7 +35,7 @@ sudo cp example.com /var/www/
 sudo chown -R www-data:www-data /var/www/example.com
 sudo chmod -R 555 /var/www/example.com
 
-wget 'https://github.com/ibinlike/vmess_tcp_caddy/blob/master/caddy.service'
+wget 'https://raw.githubusercontent.com/ibinlike/vmess_tcp_caddy/master/caddy.service'
 sudo cp caddy.service /etc/systemd/system/
 sudo chown root:root /etc/systemd/system/caddy.service
 sudo chmod 644 /etc/systemd/system/caddy.service
